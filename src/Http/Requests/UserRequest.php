a2b2a5e7bef005a43db609f1a54422439c66ddfe
Mario Patronelli <mario@raffles.com.ar>
2017-09-13 09:00:27 -0300

    Update UserRequest.php rules

--- src/Http/Requests/UserRequest.php
+++ src/Http/Requests/UserRequest.php
@@ -23,13 +23,38 @@ class UserRequest extends ActionBasedFormRequest
     public static function store()
     {
         return [
-            'email' => 'required|max:255',
+            'fax' => 'nullable|max:25',
+            'city' => 'nullable|max:50',
+            'cuit' => 'nullable|numeric|digits:11',
+            'town' => 'nullable|max:50',
+            'email' => [
+                'required',
+                'max:255',
+                Rule::unique(config('user-role-and-permission.table_name') ?: 'users'),
+            ],
             'slug' => [
                 'sometimes',
-                Rule::unique(config('user-role-and-permission.table_name')),
+                Rule::unique(config('user-role-and-permission.table_name') ?: 'users'),
             ],
-            'password' => 'required|confirmed|min:6',
-            
+            'phone' => 'nullable|max:25',
+            'state' => 'nullable|max:50',
+            'mobile' => 'nullable|max:25',
+            'city_id' => 'nullable|numeric',
+            'comment' => 'nullable|max:255',
+            'country' => 'nullable|max:50',
+            'twitter' => 'nullable|url',
+            'website' => 'nullable|url',
+            'document' => 'nullable|numeric|digits_between:6,8',
+            'facebook' => 'nullable|url',
+            'password' => 'sometimes|confirmed|min:6',
+            'position' => 'nullable|max:50',
+            'state_id' => 'nullable|numeric',
+            'zip_code' => 'nullable|alpha_num|max:25',
+            'last_name' => 'nullable|max:50',
+            'country_id' => 'nullable|numeric',
+            'first_name' => 'required|max:25',
+            'home_address' => 'nullable|max:50',
+            'legal_address' => 'nullable|max:50',
         ];
     }
 
@@ -48,6 +73,14 @@ class UserRequest extends ActionBasedFormRequest
         $updateRules = array_set($storeRules, 'slug',
             [
                 'sometimes',
+                Rule::unique(config('user-role-and-permission.table_name') ?: 'users')->ignore($id)
+            ]
+        );
+
+        $updateRules = array_set($updateRules, 'email',
+            [
+                'required',
+                'max:255',
                 Rule::unique(config('user-role-and-permission.table_name') ?: 'users')->ignore($id)
             ]
         );
